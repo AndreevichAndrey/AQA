65be2f4086c0896a6a3561aa45e786a6710fe3d0
Andrii <mr.winfast@gmail.com>
2025-09-28 18:26:37 +0200

    Add solution to logical.js

---
+++ lesson3/src/logical.js
@@ -0,0 +1,18 @@
+const num = 10;
+const str = '10';
+const bool = true;
+const undef = undefined;
+const nu = null;
+
+console.log(num == str);
+console.log(num === str);
+console.log(num != str);
+console.log(num !== str);
+console.log(num > 1);
+console.log(num < 1);
+
+console.log(bool && num > 5);
+console.log(bool || num < 5);
+
+console.log(undef == n);
+console.log(undef === n);
